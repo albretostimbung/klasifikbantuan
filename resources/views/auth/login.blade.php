2f3d5911e396cf5d98ab8751fee41cea5d7213f9
Albreto Suryanata Timbung <albreto.suryanata@ifg-life.id>
2025-03-24 01:30:28 +0700

    feat: changes logo on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,17 @@
 
 @section('content')
     <section class="py-[50px] flex flex-col items-center justify-center px-4">
-        <img src="/assets/svgs/logo-type.svg" alt="" />
+        <div class="flex items-center">
+            <svg class="w-16 h-16 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19a5 5 0 1 0 0-10 5 5 0 0 0 0 10z"></path>
+            </svg>
+            <div class="text-dark text-[24px] font-bold mt-2">Klasifikasi Penerima Bantuan</div>
+        </div>
         <div class="text-[32px] font-semibold text-dark mt-[70px]">Sign In</div>
         <p class="mt-4 text-base leading-7 text-center mb-[50px] text-grey">
-            Manage your data for<br />
-            poor people classification
+            Kelola data penduduk untuk<br />
+            klasifikasi penerima bantuan
         </p>
         <form class="w-full card" method="POST" action="{{ route('login') }}">
             @csrf
